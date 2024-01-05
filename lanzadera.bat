ec457e4e4bf1d7a6cf973722b1a7d1f43323bbee
Ramón Jesús Gómez Carmona <ramonjgomezcarmona@gmail.com>
2024-01-05 23:13:45 +0100

    Add files via upload

---
+++ lanzadera.bat
@@ -0,0 +1,21 @@
+@echo off
+
+if "%1"=="" (
+    
+	echo Uso: %0 archivo.jar
+    
+	exit /b 1
+)
+
+set "archivo=%1"
+
+if not exist "%archivo%" (
+    
+	echo El archivo "%archivo%" no existe.
+    
+	exit /b 1
+)
+
+java -jar "%archivo%"
+
+exit /b 0
